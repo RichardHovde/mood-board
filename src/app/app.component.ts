7fb70bba3bbc1972df73857991d77454ca81b83b
richarhh <richarhh@stud.ntnu.no>
2024-10-27 21:14:41 +0100

    01: Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule} from '@angular/material/tabs'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatTabsModule,
+    MatSlideToggleModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mood-board';
+  title = 'Mood board';
 }
